4a9fdcf30af0e5d384e21ccf5db5380839874659
Robi <babirak.robert0521@gmail.com>
2022-02-21 13:28:28 +0100

    alap valuek

--- java.js
+++ java.js
@@ -6,5 +6,14 @@ function szamitas()
 }
 
 function ellenőrzés(){
-    
+    var egy = document.getElementById("1").value;
+	var ketto = document.getElementById("2").value;
+	var harom = document.getElementById("3").value;
+	var negy = document.getElementById("4").value;
+	var ot = document.getElementById("5").value;
+	var hat = document.getElementById("6").value;
+	var het = document.getElementById("7").value;
+	var nyolc = document.getElementById("8").value;
+	var kilenc = document.getElementById("9").value;
+    var tiz = document.getElementById("10").value;
 }
